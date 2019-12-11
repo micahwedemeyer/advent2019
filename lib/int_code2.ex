90f72a83d209555f8eef35a082e284c2c35f1273
Micah Wedemeyer <me@micahwedemeyer.com>
2019-12-11 04:38:23 -0500

    Playing with day 5

--- lib/int_code2.ex
+++ lib/int_code2.ex
@@ -1,4 +1,29 @@
 defmodule IntCode2 do
+  def parse_instruction(instruction) do
+    digits = case digit_count(instruction) do
+      1 -> 2 # Strange case with single digit opcode and all leading zeros
+      x -> x
+    end
+    opcode = digit(instruction, 1) * 10 + digit(instruction, 0)
+
+    # Shift by 2 for the opcode digits
+    params = 2..(param_count(opcode) + 2)
+    |> Enum.take(param_count(opcode)) # Kinda weird, but required because ranges are inclusive
+    |> Enum.map(&(digit(instruction, &1)))
+    |> Enum.map(&param_mode/1)
+    |> Enum.to_list
+
+    [opcode] ++ params
+  end
+
+  def find_params(input, position, parsed_instruction) do
+  end
+
+  def int_code(input, position, 1, {op1, op2, res}) do
+    val = Enum.at(input, op1) + Enum.at(input, op2)
+    {:ok, List.replace_at(input, res, val)}
+  end
+
   def int_code(input, 1, position) do
     [op1, op2, res] = int_code_params(input, position)
     val = Enum.at(input, op1) + Enum.at(input, op2)
@@ -22,4 +47,34 @@ defmodule IntCode2 do
       Enum.at(input, position + 3)
     ]
   end
+
+  # Returns the digit at the power of 10
+  # digit(123, 0) == 3
+  # digit(123, 1) == 2
+  # digit(123, 2) == 1
+  def digit(val, pow10) do
+    shifted = div(val, pow(10, pow10))
+    rem(shifted, 10)
+  end
+
+  def  pow(n, k), do: pow(n, k, 1)
+  defp pow(_, 0, acc), do: acc
+  defp pow(n, k, acc), do: pow(n, k - 1, n * acc)
+
+  def digit_count(0), do: 1
+  def digit_count(val) when val < 0, do: digit_count(val * -1)
+  def digit_count(val) do
+    trunc(Float.ceil(:math.log10(val + 1)))
+  end
+
+  def param_mode(0), do: :p
+  def param_mode(1), do: :i
+
+  def param_count(1), do: 3
+  def param_count(2), do: 3
+  def param_count(99), do: 0
+  def param_count(_), do: :err
+
+  def val_at(_input, {:i, x}), do: x
+  def val_at(input, {:p, x}), do: Enum.at(input, x)
 end
--- test/int_code2_test.exs
+++ test/int_code2_test.exs
@@ -1,3 +1,35 @@
 defmodule IntCode2Test do
   use ExUnit.Case
+
+  test "parses instruction sets" do
+    assert IntCode2.parse_instruction(1002) == [2, :p, :i, :p]
+    assert IntCode2.parse_instruction(1) == [1, :p, :p, :p]
+    assert IntCode2.parse_instruction(99) == [99]
+  end
+
+  test "interprets parameters" do
+    assert IntCode2.find_params([1,2,3,4,5,6,7], 2, {2, :p, :i, :p}) == {2, {:p, 4}, {:i, 5}, {:p, 6}}
+  end
+
+  test "digit_count" do
+    assert IntCode2.digit_count(1000) == 4
+    assert IntCode2.digit_count(9999) == 4
+    assert IntCode2.digit_count(10000) == 5
+    assert IntCode2.digit_count(99999) == 5
+    assert IntCode2.digit_count(1) == 1
+    assert IntCode2.digit_count(0) == 1
+    assert IntCode2.digit_count(-1) == 1
+    assert IntCode2.digit_count(-10000) == 5
+  end
+
+  test "digit" do
+    assert IntCode2.digit(123, 0) == 3
+    assert IntCode2.digit(123, 1) == 2
+    assert IntCode2.digit(123, 2) == 1
+
+    # For leading zeros
+    assert IntCode2.digit(1, 1) == 0
+    assert IntCode2.digit(1, 2) == 0
+    assert IntCode2.digit(111, 3) == 0
+  end
 end
